ff2e2eeea0ff8f1bf8614908d2db9909f15d113c
LHAR <devlana@outlook.com>
2023-07-23 07:54:28 +0100

    test(expenses-feature-details.component.spec.ts): add missing provider

--- libs/expenses/feature-details/src/lib/expenses-feature-details.component.spec.ts
+++ libs/expenses/feature-details/src/lib/expenses-feature-details.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ExpensesFeatureDetailsComponent } from './expenses-feature-details.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 
 describe('ExpensesFeatureDetailsComponent', () => {
   let component: ExpensesFeatureDetailsComponent;
   let fixture: ComponentFixture<ExpensesFeatureDetailsComponent>;
+  let store: MockStore;
+  const initialState = { expenses: [] };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ExpensesFeatureDetailsComponent],
+      providers: [provideMockStore({ initialState })],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ExpensesFeatureDetailsComponent);
