f855d14b3106f5d6ec4bf907019a5d24a9814b5c
LHAR <devlana@outlook.com>
2023-07-26 07:37:29 +0100

    refactor: moved app.config to shell

--- libs/shell/feature/src/lib/app.config.ts
+++ libs/shell/feature/src/lib/app.config.ts
@@ -4,12 +4,12 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { provideStore } from '@ngrx/store';
-import { shellFeatureRoutes } from '@snarbanking-workspace/shell/feature';
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
 import { provideEffects } from '@ngrx/effects';
+import { shellFeatureRoutes } from './lib.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
