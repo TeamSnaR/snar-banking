5e3e85fb573920cc0822d224dbcef7316a3ec448
LHAR <devlana@outlook.com>
2023-07-29 22:18:46 +0100

    feat: added new default page title strategy

---
+++ libs/shell/feature/src/lib/snarbanking-default-page-title.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+
+@Injectable({ providedIn: 'root' })
+export class SnarbankingDefaultPageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`Snar Banking | ${title}`);
+    }
+  }
+}
