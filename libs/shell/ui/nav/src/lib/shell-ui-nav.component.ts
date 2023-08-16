79fe2e7af3acfb91b7440d2f401d0e1c4ce1888b
LHAR <devlana@outlook.com>
2023-08-16 05:34:53 +0100

    feat: add toggle control on user menu

--- libs/shell/ui/nav/src/lib/shell-ui-nav.component.ts
+++ libs/shell/ui/nav/src/lib/shell-ui-nav.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'snarbanking-workspace-shell-ui-nav',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, OverlayModule],
   templateUrl: './shell-ui-nav.component.html',
   styles: [
     `
@@ -16,4 +17,11 @@ import { RouterModule } from '@angular/router';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShellUiNavComponent {}
+export class ShellUiNavComponent {
+  showUserMenu = false;
+
+  toggleUserMenu() {
+    this.showUserMenu = !this.showUserMenu;
+    console.log('toggleUserMenu', this.showUserMenu);
+  }
+}
