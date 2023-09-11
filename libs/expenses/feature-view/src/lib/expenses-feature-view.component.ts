1b45ad9d2cfdd724796d75c2ce50550d90d83c5b
LHAR <devlana@outlook.com>
2023-09-11 21:37:24 +0100

    refactor: config custom dialogContainer

--- libs/expenses/feature-view/src/lib/expenses-feature-view.component.ts
+++ libs/expenses/feature-view/src/lib/expenses-feature-view.component.ts
@@ -1,6 +1,8 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  EnvironmentInjector,
+  StaticProvider,
   Type,
   inject,
 } from '@angular/core';
@@ -8,13 +10,23 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import * as fromSelectors from '@snarbanking-workspace/expenses/data-access';
 import { RouterModule } from '@angular/router';
-import { DialogModule } from '@angular/cdk/dialog';
-import { ExpensesUiExpenseFormComponent } from '@snarbanking-workspace/expenses/ui-expense-form';
+import {
+  Dialog,
+  DialogConfig,
+  DialogModule,
+  DialogRef,
+} from '@angular/cdk/dialog';
+import {
+  ExpenseFormContainerComponent,
+  ExpensesUiExpenseFormComponent,
+} from '@snarbanking-workspace/expenses/ui-expense-form';
 import { UtilDialogService } from '@snarbanking-workspace/shared/util-dialog';
+import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { BasePortalOutlet } from '@angular/cdk/portal';
 @Component({
   selector: 'snarbanking-workspace-expenses-feature-view',
   standalone: true,
-  imports: [CommonModule, RouterModule, DialogModule],
+  imports: [CommonModule, RouterModule, DialogModule, OverlayModule],
   templateUrl: './expenses-feature-view.component.html',
   styles: [
     `
@@ -28,11 +40,26 @@ import { UtilDialogService } from '@snarbanking-workspace/shared/util-dialog';
 export class ExpensesFeatureViewComponent {
   public vm$ = this.expensesStore.select(fromSelectors.selectExpensesVm);
   utilDialogService = inject(UtilDialogService);
+  #dialog = inject(Dialog);
+  #overlay = inject(Overlay);
   constructor(public expensesStore: Store) {}
   createNewExpense(component: Type<unknown> = ExpensesUiExpenseFormComponent) {
     // this.expensesStore.dispatch(
     //   fromActions.createNewExpense({ component: this.testDialog })
     // );
-    this.utilDialogService.open(component);
+    // this.utilDialogService.open(component);
+    this.#dialog.open(component, {
+      positionStrategy: this.#overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .right('0'),
+      container: {
+        type: ExpenseFormContainerComponent,
+        providers: (config) => {
+          return [];
+        },
+      },
+    });
   }
 }
--- libs/expenses/ui-expense-form/src/lib/expenses-ui-expense-form.component.ts
+++ libs/expenses/ui-expense-form/src/lib/expenses-ui-expense-form.component.ts
@@ -1,17 +1,13 @@
 import {
-  AfterContentChecked,
-  AfterContentInit,
-  AfterViewChecked,
-  AfterViewInit,
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   Input,
+  ViewChild,
   inject,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DIALOG_DATA } from '@angular/cdk/dialog';
-import { Observable, of } from 'rxjs';
+import { CdkDialogContainer, DIALOG_DATA } from '@angular/cdk/dialog';
+import { CdkPortalOutlet, PortalModule } from '@angular/cdk/portal';
 
 @Component({
   selector: 'snarbanking-workspace-expenses-ui-expense-form',
@@ -30,4 +26,71 @@ import { Observable, of } from 'rxjs';
 export class ExpensesUiExpenseFormComponent {
   @Input() contentData: { text: string } | undefined;
   dialogData = inject(DIALOG_DATA);
+}
+
+@Component({
+  selector: 'snarbanking-workspace-expenses-ui-expense-form-container',
+  standalone: true,
+  imports: [PortalModule, CommonModule],
+  template: `
+    <div class="flex min-h-0 flex-1 flex-col overflow-y-scroll pt-6">
+      <header class="px-4 sm:px-6">
+        <div class="flex items-start justify-between">
+          <h2>Header section</h2>
+          <div class="ml-3 flex h-7 items-center">
+            <button
+              type="button"
+              class="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              <span class="absolute -inset-2.5"></span>
+              <span class="sr-only">Close panel</span>
+              <svg
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
+      <div class="relative mt-6 flex-1 px-4 sm:px-6">
+        <ng-container *cdkPortalOutlet></ng-container>
+      </div>
+    </div>
+    <footer class="flex flex-shrink-0 justify-end px-4 py-4">
+      <button
+        type="button"
+        class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
+      >
+        Cancel
+      </button>
+      <button
+        type="submit"
+        class="ml-4 inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+      >
+        Save
+      </button>
+    </footer>
+  `,
+  styles: [
+    `
+      :host {
+        @apply w-full h-full flex flex-col divide-y divide-gray-200 bg-white shadow-xl;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ExpenseFormContainerComponent extends CdkDialogContainer {
+  @ViewChild(CdkPortalOutlet, { static: true })
+  override _portalOutlet!: CdkPortalOutlet;
 }
